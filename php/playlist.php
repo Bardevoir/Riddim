c11c81bd91c3cb9190004c0d329ed68d396a9dc3
Bardevoir <nuttyboy31nicolas.huard@gmail.Com>
2018-11-16 12:35:53 +0100

    playlist update

--- php/playlist.php
+++ php/playlist.php
@@ -49,7 +49,7 @@
 </nav>
 
     <div class="container">
-    <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/514227969&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+    <iframe width="100%" height="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/playe r/?url=https%3A//api.soundcloud.com/playlists/514227969&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     </div>
 </body>
 
